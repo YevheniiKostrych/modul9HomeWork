ba4353e559f26e554396b520e0b994bf10fe5757
YevheniiKostrych <156820347+YevheniiKostrych@users.noreply.github.com>
2024-06-03 13:03:43 +0200

    Add files via upload

--- MyQueue.java
+++ MyQueue.java
@@ -1,41 +1,29 @@
 package modul9.myQueue;
-
-
-
 public class MyQueue <T> {
     transient Node<T> head;
-
     transient Node<T> tail;
     private int size;
-
     public MyQueue() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
-
     private static class Node<T> {
         private T value;
         private Node<T> next;
-
         private Node<T> prev;
-
 
         public Node(T value) {
             this.value = value;
         }
-
         @Override
         public String toString() {
             return "Node{" +
                     "value=" + value +
                     '}';
         }
-    }
-
-    public void add(T t) {
-
-            MyQueue.Node<T> newNode = new MyQueue.Node<>(t);
+    }public void add(T t) {
+        MyQueue.Node<T> newNode = new MyQueue.Node<>(t);
             if (head == null) {
                 head = newNode;
                 tail = newNode;
@@ -44,33 +32,27 @@ public class MyQueue <T> {
                 tail.next = newNode;
                 newNode.prev = tail;
                 tail = newNode;
-
-
             }
             size++;
-
-
-
     }
-  public  Object peek(){
+  public  T peek(){
         if (head!=null){
       MyQueue.Node<T> current = head;
         return current.value;}
         else return null;
   }
-   public Object poll(){
+   public T poll(){
        if (head!=null){
            MyQueue.Node<T> current = head;
 
-          Object result= current.value;
+          T result= current.value;
            head = head.next;
            size--;
-           return result;
+           return  result;
        }
        else
            return null;
-
-   }
+    }
     public int size() {
         return size;
     }
@@ -79,15 +61,12 @@ public class MyQueue <T> {
         head = null;
         tail = null;
         size = 0;
-
     }
-
     @Override
     public String toString() {
         if (size == 0) {
             return "[]";
         }
-
         StringBuilder sb = new StringBuilder("[");
         MyQueue.Node current = head;
         while (current != null) {
