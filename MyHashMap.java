a0d77d912bbf87f9c993d7d53858437aac519fc3
YevheniiKostrych <156820347+YevheniiKostrych@users.noreply.github.com>
2024-06-03 13:03:21 +0200

    Add files via upload

--- MyHashMap.java
+++ MyHashMap.java
@@ -1,22 +1,26 @@
 package modul9.myHashMap;
 
 import java.util.Arrays;
-
 public class MyHashMap<K,V>{
     private static final int DEFAULT_SIZE=8;
     private int size=0;
 
-
     private Entry<K,V>[] context=new Entry[DEFAULT_SIZE];
     public void put(K key,V value){
-        int index=calculateIndex(key.hashCode());
+
+        int index=calculateIndex(key);
         Entry<K,V>newEntry=new Entry<>(key,value);
         if (context[index]==null){
             context[index]=newEntry;
 
         } else {
+
             Entry<K, V> current = context[index];
             while (current != null) {
+                if (key == null) {
+                    if (key==null) ;
+                    current.value = value;
+                    return;}
                 if (current.key.equals(key)) {
                     current.value = value;
                     return;
@@ -31,20 +35,20 @@ public class MyHashMap<K,V>{
         size++;
     }
     public V get (K key ){
-        int index =calculateIndex(key.hashCode());
-        if (context[index].key.equals(key)){
-            return context[index].value;
-        }else {
-            if (context[index].next!=null){
-            Entry<K, V> current = context[index];
-            current=current.next;
-            return current.value; }
+        int index =calculateIndex(key);
+        Entry<K, V> current = context[index];
 
+        while (current != null) {
+            if (current.key.equals(key)) {
+                return current.value;
+            }
+            current = current.next;
         }
-        return  null;
+        return null;
     }
-   public void remove(K key){
-       int index =calculateIndex(key.hashCode());
+
+    public void remove(K key){
+       int index =calculateIndex(key);
        Entry<K, V> current = context[index];
        Entry<K, V> previous = null;
        while (current != null) {
@@ -67,14 +71,11 @@ public class MyHashMap<K,V>{
         return size;
     }
     public void clear() {
-
         context = new Entry[DEFAULT_SIZE];
         size = 0;
-
     }
-    private int calculateIndex(int hashCode){
-        return Math.abs(hashCode%context.length);
-
+    private int calculateIndex(K key){
+        return key == null ? 0 : Math.abs(key.hashCode() % context.length);
     }
 
     @Override
@@ -93,19 +94,13 @@ public class MyHashMap<K,V>{
                 entry = entry.next;
             }
         }
-
         sb.append("}");
         return sb.toString();
     }
-
     static class Entry <K,V>{
         K key;
         V value;
         Entry <K,V> next;
-
-
-
-
         public Entry(K key, V value) {
             this.key = key;
             this.value = value;
