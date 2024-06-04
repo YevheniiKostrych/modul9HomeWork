8446fc5b335b541860eeb3699f9a89d614b7319a
YevheniiKostrych <156820347+YevheniiKostrych@users.noreply.github.com>
2024-06-04 10:04:26 +0200

    Add files via upload

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -2,63 +2,45 @@ package modul9.myLinkedList;
 
 public class MyLinkedList <T>{
    transient MyLinkedList.Node<T> head;
-
-    transient MyLinkedList.Node<T> tail;
+   transient MyLinkedList.Node<T> tail;
     private int size ;
     public MyLinkedList() {
         this.head = null;
         this.tail = null;
         this.size = 0;
     }
-
-
-
     public void add(T t ){
         Node<T> newNode=new Node<>(t);
         if (head==null){
            head=newNode;
            tail=newNode;
         }else {
-
-          tail.next=newNode;
-          newNode.prev=tail;
-          tail=newNode;
-
-
+            tail.next=newNode;
+            newNode.prev=tail;
+            tail=newNode;
         }
     size++;
     }
-
-
-
-
-    public Object get(int index) {
+    public T get(int index) {
         if (index < 0 || index >= size) {
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         }
-
         Node<T> current = head;
         for (int i = 0; i < index; i++) {
             current = current.next;
         }
-
         return current.value;
     }
-
-   public void remove(int index) {
+    public void remove(int index) {
        if (index < 0 || index >= size) {
            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
        }
-
        Node<T> currentNode = head;
-
        if (index == 0) {
-
            head = head.next;
            if (head != null) {
                head.prev = null;
            } else {
-
                tail = null;
            }
        } else {
@@ -69,15 +51,12 @@ public class MyLinkedList <T>{
            if (currentNode.next != null) {
                currentNode.next.prev = currentNode.prev;
            } else {
-
                tail = currentNode.prev;
            }
        }
-
        size--;
    }
-
-    public int size() {
+   public int size() {
         return size;
     }
     public void clear() {
@@ -87,13 +66,11 @@ public class MyLinkedList <T>{
         size = 0;
 
     }
-
     @Override
     public String toString() {
         if (size == 0) {
             return "[]";
         }
-
         StringBuilder sb = new StringBuilder("[");
         Node current = head;
         while (current != null) {
@@ -106,20 +83,13 @@ public class MyLinkedList <T>{
         sb.append("]");
         return sb.toString();
     }
-
     private static class Node<T>{
         private T value;
         private Node<T> next;
 
        private Node<T> prev;
-
-
-
-
        public Node(T value){
             this.value=value;}
-
-
         @Override
         public String toString() {
             return "Node{" +
@@ -129,8 +99,7 @@ public class MyLinkedList <T>{
                     '}';
         }
     }
-
-    }
+}
 
 
 
